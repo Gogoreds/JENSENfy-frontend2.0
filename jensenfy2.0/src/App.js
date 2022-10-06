7959571030b28c5c06d18b9b2e483927fde47eaf
Mohammad Arefi <m-arefi@live.com>
2022-10-07 01:03:50 +0200

    Fonts imported

--- jensenfy2.0/src/App.js
+++ jensenfy2.0/src/App.js
@@ -10,35 +10,6 @@ import { accessToken, logout, getCurrentUserProfile } from "./spotify";
 import { catchErrors } from "./utils";
 import styled, { createGlobalStyle } from "styled-components/macro";
 
-const GlobalStyle = createGlobalStyle`
-:root {
-  --color-white: #fff;
-  --color-black: #000;
-  --color-grey: #b3b3b3;
-  --color-green: #1db954;
-  --color-dark-green: #1ed760;
-  --color-light-green: #a7f0ba;
-  --color-light-red: #ff4d4d;
-  --color-blue: #1db9ff;
-  --color-dark-blue: #1ed7ff;
-  --color-light-blue: #a7f0ff;
-}
-
-html {
-  box-sizing: border-box;
-}
-
-*, *:before, *:after {
-  box-sizing: inherit;
-}
-
-body {
-  margin: 0;
-  padding: 0;
-  background-color: black;
-  color: white;
-}`;
-
 const StyledLoginButton = styled.a`
   background-color: green;
   color: white;
