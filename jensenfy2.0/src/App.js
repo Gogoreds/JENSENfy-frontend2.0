1a8c1132883c2b13183ae016b357ea304c4af0be
Mohammad Arefi <m-arefi@live.com>
2022-10-07 00:25:26 +0200

    react router dom v5

--- jensenfy2.0/src/App.js
+++ jensenfy2.0/src/App.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import { accessToken, logout, getCurrentUserProfile } from "./spotify";
 import { catchErrors } from "./utils";
 import "./App.css";
@@ -23,22 +24,40 @@ function App() {
       <header className="App-header">
         {!token ? (
           <a className="App-link" href="http://localhost:8888/login">
-            Log in with Spotify
+            Log in to Spotify
           </a>
         ) : (
-          <>
-            <p>Logged in</p>
-            <button onClick={logout}>Log out</button>
-            {profile && (
-              <div>
-                <h1>{profile.display_name}</h1>
-                <p>{profile.followers.total} Followers</p>
-                {profile.images.length && profile.images[0].url && (
-                  <img src={profile.images[0].url} alt="Avatar" />
-                )}
-              </div>
-            )}
-          </>
+          <Router>
+            <Switch>
+              <Route path="/top-artists">
+                <h1>Top Artists</h1>
+              </Route>
+              <Route path="/top-tracks">
+                <h1>Top Tracks</h1>
+              </Route>
+              <Route path="/playlists/:id">
+                <h1>Playlist</h1>
+              </Route>
+              <Route path="/playlists">
+                <h1>Playlists</h1>
+              </Route>
+              <Route path="/">
+                <>
+                  <button onClick={logout}>Log Out</button>
+
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.images.length && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar" />
+                      )}
+                    </div>
+                  )}
+                </>
+              </Route>
+            </Switch>
+          </Router>
         )}
       </header>
     </div>
