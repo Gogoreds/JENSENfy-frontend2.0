f7c0ee052b9ceb353619b2237a2f570e5716e722
Mohammad Arefi <m-arefi@live.com>
2022-10-06 23:55:56 +0200

    Profile info routed

--- jensenfy2.0/src/App.js
+++ jensenfy2.0/src/App.js
@@ -33,6 +33,15 @@ function App() {
           <>
             <p>Logged in</p>
             <button onClick={logout}>Log out</button>
+            {profile && (
+              <div>
+                <h1>{profile.display_name}</h1>
+                <p>{profile.followers.total} Followers</p>
+                {profile.images.length && profile.images[0].url && (
+                  <img src={profile.images[0].url} alt="Avatar" />
+                )}
+              </div>
+            )}
           </>
         )}
       </header>
