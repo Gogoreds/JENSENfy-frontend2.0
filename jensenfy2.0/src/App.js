d9fb0e38f09ef8a8a4bec57095d3fe9f98a40271
Mohammad Arefi <m-arefi@live.com>
2022-10-06 22:54:01 +0200

    SpotifyJS init

--- jensenfy2.0/src/App.js
+++ jensenfy2.0/src/App.js
@@ -4,11 +4,6 @@ import "./App.css";
 //funny facts
 function App() {
   useEffect(() => {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    const accessToken = urlParams.get("access_token");
-    const refreshToken = urlParams.get("refresh_token");
-
     console.log(accessToken);
     console.log(refreshToken);
 
