539556a87f001f7b03af3216dd1d15b783ffc500
Mohammad Arefi <m-arefi@live.com>
2022-10-07 01:45:54 +0200

    Playlists added

--- jensenfy2.0/src/pages/Profile.js
+++ jensenfy2.0/src/pages/Profile.js
@@ -14,8 +14,6 @@ const Profile = () => {
 
       const userPlaylists = await getCurrentUserPlaylists();
       setPlaylists(userPlaylists.data);
-
-      console.log(userPlaylists.data);
     };
 
     catchErrors(fetchData());
@@ -38,6 +36,12 @@ const Profile = () => {
                 <div className="header__overline">Profile</div>
                 <h1 className="header__name">{profile.display_name}</h1>
                 <p className="header__meta">
+                  {playlists && (
+                    <span>
+                      {playlists.total} Playlist
+                      {playlists.total !== 1 ? "s" : ""}
+                    </span>
+                  )}
                   <span>
                     {profile.followers.total} Follower
                     {profile.followers.total !== 1 ? "s" : ""}
