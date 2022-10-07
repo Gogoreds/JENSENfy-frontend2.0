7c954d91881120c389bfea0d9bc27e0e1849f482
Mohammad Arefi <m-arefi@live.com>
2022-10-07 04:28:48 +0200

    Loader and RangeButtons recreated

--- jensenfy2.0/src/components/TimeRangeButtons.js
+++ jensenfy2.0/src/components/TimeRangeButtons.js
@@ -1,10 +1,31 @@
-import { StyledRangeButtons } from "../styles"
+import { StyledRangeButtons } from "../styles";
 
-const TimeRangeRangeButtons = ({ activeRange, setActiveRange }) => (
+const TimeRangeButtons = ({ activeRange, setActiveRange }) => (
   <StyledRangeButtons>
-    <li><button className={activeRange === 'short' ? 'active' : ''} onClick={() => setActiveRange(`short`)}>This month</button></li>
-    <li><button className={activeRange === 'medium' ? 'active' : ''} onClick={() => setActiveRange(`medium`)}>Last 6 month</button></li>
-    <li><button className={activeRange === 'long' ? 'active' : ''} onClick={() => setActiveRange(`long`)}>All time</button></li>
+    <li>
+      <button
+        className={activeRange === "short" ? "active" : ""}
+        onClick={() => setActiveRange(`short`)}
+      >
+        This month
+      </button>
+    </li>
+    <li>
+      <button
+        className={activeRange === "medium" ? "active" : ""}
+        onClick={() => setActiveRange(`medium`)}
+      >
+        Last 6 month
+      </button>
+    </li>
+    <li>
+      <button
+        className={activeRange === "long" ? "active" : ""}
+        onClick={() => setActiveRange(`long`)}
+      >
+        All time
+      </button>
+    </li>
   </StyledRangeButtons>
-)
-export default TimeRangeRangeButtons
+);
+export default TimeRangeButtons;
