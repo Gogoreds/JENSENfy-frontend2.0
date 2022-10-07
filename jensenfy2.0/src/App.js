16863bab874fc24468bbfff5006efd0a6ee3dbe4
Mohammad Arefi <m-arefi@live.com>
2022-10-07 05:07:06 +0200

    Playlist init

--- jensenfy2.0/src/App.js
+++ jensenfy2.0/src/App.js
@@ -9,7 +9,14 @@ import {
 import { accessToken, logout, getCurrentUserProfile } from "./spotify";
 import { catchErrors } from "./utils";
 import { GlobalStyle } from "./styles";
-import { Login, Profile, TopArtists, TopTracks, Playlists } from "./pages";
+import {
+  Login,
+  Profile,
+  TopArtists,
+  TopTracks,
+  Playlists,
+  Playlist,
+} from "./pages";
 import styled from "styled-components/macro";
 
 const StyledLogoutButton = styled.button`
@@ -72,10 +79,10 @@ function App() {
                   <TopTracks />
                 </Route>
                 <Route path="/playlists/:id">
-                  <Playlists />
+                  <Playlist />
                 </Route>
                 <Route path="/playlists">
-                  <h1>Playlists</h1>
+                  <Playlists />
                 </Route>
                 <Route path="/">
                   <Profile />
